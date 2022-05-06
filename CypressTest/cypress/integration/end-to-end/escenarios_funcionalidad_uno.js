f07a69250ba2680ff0ed9dee7b07248a1cd894c8
Santiago-Cortes <santiago.cortes@coxautoinc.com>
2022-05-05 20:54:02 -0500

    F001E02: Escenario Implementado

--- CypressTest/cypress/integration/end-to-end/escenarios_funcionalidad_uno.js
+++ CypressTest/cypress/integration/end-to-end/escenarios_funcionalidad_uno.js
@@ -1,10 +1,11 @@
 import { AdminDashboard } from "../pages/admin/dashboard";
 import { Login } from "../pages/admin/login";
-import { Post } from "../pages/admin/editor";
+import { Post, Page } from "../pages/admin/editor";
 
 const dashboard = new AdminDashboard();
 const login = new Login();
 const post = new Post();
+const page = new Page();
 
 
 /**
@@ -41,6 +42,29 @@ describe('Funcionalidad: Creación de Post/Pages', () =>{
             pItem.click();
             cy.wait(300);
             post.readArticle((txt) => expect(txt).to.equal(article));
+        });
+    });
+
+    it('E002: Creación de un Page Básico', () => {
+        // GIVEN (additional to the login)
+
+        // WHEN the admin goes to create a Page, writes a title
+        // and writes text content
+        dashboard.getPages();
+        page.createNewPage();
+
+        let title = page.writeTitle();
+        let article = page.writeArticle();
+
+        // THEN the page should appear as the first item in the
+        // list, and the article's context should be the same as
+        // the one written before
+        page.exitEditorWithBackButton();
+        page.getFirstFromListByTitle(title, (pItem) => {
+            expect(pItem).to.exist;
+            pItem.click();
+            cy.wait(300);
+            page.readArticle((txt) => expect(txt).to.equal(article));
         });
     });
 });
