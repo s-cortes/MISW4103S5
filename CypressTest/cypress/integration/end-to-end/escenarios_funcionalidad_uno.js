d0e4897cb1f9f01caaeacde27458323f1fa3350b
Santiago-Cortes <santiago.cortes@coxautoinc.com>
2022-05-05 20:53:10 -0500

    F001E01: Escenario Implementado

--- CypressTest/cypress/integration/end-to-end/escenarios_funcionalidad_uno.js
+++ CypressTest/cypress/integration/end-to-end/escenarios_funcionalidad_uno.js
@@ -1,10 +1,11 @@
 import { AdminDashboard } from "../pages/admin/dashboard";
 import { Login } from "../pages/admin/login";
-import { Post } from "../pages/admin/post";
+import { Post } from "../pages/admin/editor";
 
 const dashboard = new AdminDashboard();
 const login = new Login();
 const post = new Post();
+
 
 /**
  * Agrupación de Escenarios prof Funcionalidad
@@ -15,11 +16,11 @@ describe('Funcionalidad: Creación de Post/Pages', () =>{
     before(() => {
         // GIVEN that the admin user logs-in to ghost
         login.login('admin.user@test.com', 'Admin@Test$MISW4103');
-        dashboard.navigate();
     });
 
     beforeEach(() => {
-        Cypress.Cookies.preserveOnce('ghost-admin-api-session')
+        Cypress.Cookies.preserveOnce('ghost-admin-api-session');
+        dashboard.navigate();
     });
 
     it('E001: Creación de un Post Básico', () => {
@@ -28,12 +29,18 @@ describe('Funcionalidad: Creación de Post/Pages', () =>{
         // WHEN the admin goes to create a Post, writes a title
         // and writes text content
         dashboard.createPost();
-        let title = post.writePostTitle();
-        let article = post.writePostArticle();
+        let title = post.writeTitle();
+        let article = post.writeArticle();
 
         // THEN the post should appear as the first item in the
-        // Post list when he exits the editor.
+        // list, and the article's context should be the same as
+        // the one written before
         post.exitEditorWithBackButton();
-        post.getPostFromListByTitle(title, (post) => expect(post).to.exist);
+        post.getPostFromListByTitle(title, (pItem) => {
+            expect(pItem).to.exist;
+            pItem.click();
+            cy.wait(300);
+            post.readArticle((txt) => expect(txt).to.equal(article));
+        });
     });
 });
